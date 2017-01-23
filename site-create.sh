ea6d6fa05e4a448151d6a55cf2fef287becf597d
mirocow <Mirocow@users.noreply.github.com>
2017-01-23 22:34:47 +0300

    some fixes

--- site-create.sh
+++ site-create.sh
@@ -11,43 +11,39 @@ function create_site()
 {
 
         site_name=$HOST
-				site_alias=$ALIAS
+        site_alias=$ALIAS
         site_addr=$IP
         password=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 
-        if [ -z $site_alias ]; then
-						site_alias=$site_name
-				fi
-
-				if [ -d /home/${site_name} ]; then
-						if [ $SET_PASSWORD -eq 1 ]; then
-								echo ${site_name}:${password} | chpasswd
-								usermod  -s /bin/bash ${site_name}
-						else
-								password='[without changes]'
-								echo "User's password is not updated"
-						fi						
-				else
-						mkdir /home/${site_name}
-						mkdir /home/${site_name}/logs
-						mkdir /home/${site_name}/httpdocs
-						mkdir /home/${site_name}/httpdocs/web
-						useradd -d /home/${site_name} -s /bin/bash ${site_name}
-						usermod -G www-data ${site_name}
-						echo ${site_name}:${password} | chpasswd
-						mkdir /home/${site_name}/.ssh
-						chmod 0700 /home/${site_name}/.ssh
-						ssh-keygen -t rsa -N "${site_name}" -f /home/${site_name}/.ssh/id_rsa
-						chmod 0600 /home/${site_name}/.ssh/id_rsa
-						ssh-keygen -t dsa -N "${site_name}" -f /home/${site_name}/.ssh/id_dsa
-						chmod 0600 /home/${site_name}/.ssh/id_dsa
-						echo  "<?php phpinfo();" > /home/${site_name}/httpdocs/web/index.php
-						if [ $LOCK -eq 1 ]; then
-								authpassword=$(date +%s | sha256sum | base64 | head -c 6 ; echo)
-								php -r "echo 'admin:' . crypt('${authpassword}', 'salt') . ': Web auth for ${site_name}';" > /home/${site_name}/authfile
-						fi
-						chown ${site_name}:www-data -R /home/${site_name}
-				fi
+        if [ -d /home/${site_name} ]; then
+		if [ $SET_PASSWORD -eq 1 ]; then
+		echo ${site_name}:${password} | chpasswd
+		usermod  -s /bin/bash ${site_name}
+		else
+		password='[without changes]'
+		echo "User's password is not updated"
+		fi						
+        else
+		mkdir /home/${site_name}
+		mkdir /home/${site_name}/logs
+		mkdir /home/${site_name}/httpdocs
+		mkdir /home/${site_name}/httpdocs/web
+		useradd -d /home/${site_name} -s /bin/bash ${site_name}
+		usermod -G www-data ${site_name}
+		echo ${site_name}:${password} | chpasswd
+		mkdir /home/${site_name}/.ssh
+		chmod 0700 /home/${site_name}/.ssh
+		ssh-keygen -t rsa -N "${site_name}" -f /home/${site_name}/.ssh/id_rsa
+		chmod 0600 /home/${site_name}/.ssh/id_rsa
+		ssh-keygen -t dsa -N "${site_name}" -f /home/${site_name}/.ssh/id_dsa
+		chmod 0600 /home/${site_name}/.ssh/id_dsa
+		echo  "<?php phpinfo();" > /home/${site_name}/httpdocs/web/index.php
+		if [ $LOCK -eq 1 ]; then
+		authpassword=$(date +%s | sha256sum | base64 | head -c 6 ; echo)
+		php -r "echo 'admin:' . crypt('${authpassword}', 'salt') . ': Web auth for ${site_name}';" > /home/${site_name}/authfile
+		fi
+		chown ${site_name}:www-data -R /home/${site_name}
+        fi
 
         if [ $APACHE -eq 1 ]; then
 
@@ -312,17 +308,20 @@ server {
         echo "SSH Public file: /home/${site_name}/.ssh/id_rsa.pub"
         echo "Servers:"
         echo "Site name: ${site_name} (${IP})"
-				echo "Site alias: ${site_alias}"
+	
+	if [ ! -z $site_alias ]; then
+          echo "Site alias: ${site_alias}"
+	fi
 				
-				if [ $REDIRECT = 'site-www' ]; then
-        echo "Use redirect from ${site_name} to ${server_name}"
-				fi				
-				if [ $REDIRECT = 'www-site' ]; then
-        echo "Use redirect from ${site_name} to ${server_name}"
-				fi
-				if [ $REDIRECT = 'off' ]; then
-        echo "Redirect disabled. use only ${server_name}"
-				fi
+        if [ $REDIRECT = 'site-www' ]; then
+          echo "Use redirect from ${site_name} to ${server_name}"
+        fi				
+        if [ $REDIRECT = 'www-site' ]; then
+          echo "Use redirect from ${site_name} to ${server_name}"
+        fi
+        if [ $REDIRECT = 'off' ]; then
+          echo "Redirect disabled. use only ${server_name}"
+        fi
  
         echo "Site root: /home/${site_name}/httpdocs/web"
         echo "Site logs path: /home/${site_name}/logs"
@@ -368,10 +367,10 @@ OPTIONS:
    --host=                  Host name without www (Example: --host=myhost.com)
    --ip=                    IP address, default usage 80 (Example: --ip=127.0.0.1:8080)
    --redirect=              WWW redirect add (Example: --redirect=www-site or --redirect=site-www or disable redirect --redirect=off)
-	 --alias=                 Set Nginx alias (Examle: --alias="alias1 alias2 etc")
+   --alias=                 Set Nginx alias (Examle: --alias="alias1 alias2 etc")
    --apache                 Usage apache back-end
    --awstats                Usage awstats
-	 --dont-change-password   Usage for change user password (Default: 1. Usage only for update)
+   --dont-change-password   Usage for change user password (Default: 1. Usage only for update)
    -5 | --php5              Usage PHP 5.x
    -7 | --php7              Usage PHP 7.x
    -l | --lock              Usage Nginx HTTP Auth basic	 
@@ -379,7 +378,7 @@ OPTIONS:
 
 EXAMPLES:
    bash site-create.sh --host="mirocow.com" --ip="192.168.1.131:8082"
-	 bash site-create.sh --host="mirocow.com" --alias="c1.mirocow.com c2.mirocow.com"
+   bash site-create.sh --host="mirocow.com" --alias="c1.mirocow.com c2.mirocow.com"
 
 EOF
 }
@@ -443,8 +442,8 @@ do
             shift
         ;;
         -h | --help)
-                usage
-                exit
+            usage
+            exit
         ;;
         *)
         # unknown option
